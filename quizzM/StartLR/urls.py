cbc4afef1ae73150ca64a5ce4cceea372c88f90b
Ar <aram.davtyan05@mail.ru>
2024-04-22 20:00:09 +0400

    Add new features

--- quizzM/StartLR/urls.py
+++ quizzM/StartLR/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import login_view,register_view,success_view, explore_quizzes, create_quiz, view_results
+from .views import login_view,register_view,success_view, explore_quizzes, create_quiz, view_results, add_question
 
 urlpatterns = [
     path('', login_view, name='login'),
@@ -8,4 +8,6 @@ urlpatterns = [
     path('explore/', explore_quizzes, name='explore_quizzes'),
     path('create/', create_quiz, name='create_quiz'),
     path('results/', view_results, name='view_results'),
+    path('add_question/<str:quiz_title>/', add_question, name='add_question'),
+
 ]
--- quizzM/StartLR/views.py
+++ quizzM/StartLR/views.py
@@ -5,7 +5,6 @@ from .models import Quiz, Question, Option
 from .forms import QuizForm, QuestionForm, OptionFormSet
 
 
-
 def login_view(request):
      if request.method == 'POST':
           form = AuthenticationForm(request, request.POST)
@@ -38,34 +37,49 @@ def success_view(request):
 def explore_quizzes(request):
     return render(request, 'explore_quizzes.html')
 
-def create_quiz(request):
-    return render(request, 'create_quiz.html')
-
 def view_results(request):
     return render(request, 'view_results.html')
 
 
-def create_quiz(request):
+def add_question(request, quiz_title):
     if request.method == 'POST':
+        # Process the form submission
         quiz_form = QuizForm(request.POST)
         question_form = QuestionForm(request.POST)
         option_formset = OptionFormSet(request.POST)
 
         if quiz_form.is_valid() and question_form.is_valid() and option_formset.is_valid():
-            quiz = quiz_form.save()
+            quiz = quiz_form.save() 
             question = question_form.save(commit=False)
             question.quiz = quiz
             question.save()
-            option_formset.instance = question
-            option_formset.save()
-            return redirect('create_quiz')  
+
+            correct_option_id = int(request.POST.get('correct_option'))
+
+            for option_form in option_formset:
+                option = option_form.save(commit=False)
+                option.question = question
+                option.is_correct = (option_form.instance.id == correct_option_id)
+                option.save()
+
+            return redirect('add_question', quiz_title=quiz_title) 
     else:
-        quiz_form = QuizForm()
+        quiz_form = QuizForm(initial={'title': quiz_title}) 
         question_form = QuestionForm()
         option_formset = OptionFormSet()
     
-    return render(request, 'create_quiz.html', {
+    return render(request, 'add_question.html', {
+        'quiz_title': quiz_title,
         'quiz_form': quiz_form,
         'question_form': question_form,
         'option_formset': option_formset,
     })
+
+
+def create_quiz(request):
+    if request.method == 'POST':
+        quiz_title = request.POST.get('quiz_title')
+        if quiz_title:
+            return redirect('add_question', quiz_title=quiz_title)
+    return render(request, 'create_quiz.html')
+
